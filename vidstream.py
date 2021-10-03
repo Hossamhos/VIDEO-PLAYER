9220653c9591b6a6913bcc03b3e18f63060ba7df
the-noobest-pro <87678768+the-noobest-pro@users.noreply.github.com>
2021-10-03 22:14:45 +0530

    Create vidstream.py

---
+++ vidstream.py
@@ -0,0 +1,89 @@
+import os
+import re
+import asyncio
+from pyrogram import Client, filters, idle
+from pyrogram.types import Message
+from pytgcalls import idle as pyidle
+from pytgcalls import PyTgCalls
+from pytgcalls import StreamType
+from pytgcalls.types.input_stream import AudioVideoPiped
+from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityAudio
+from pytgcalls.types.input_stream.quality import HighQualityVideo, MediumQualityVideo, LowQualityVideo
+from youtube_dl import YoutubeDL
+from youtubesearchpython import VideosSearch
+
+# YTDL
+opts = {"format": "best[height=?720]/best", "noplaylist": True}
+ydl = YoutubeDL(opts)
+
+# Client and PyTgCalls
+API_ID = int(os.getenv("API_ID", "6"))
+API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+SESSION = os.getenv("SESSION")
+CHAT = int(os.getenv("CHAT"))
+HNDLR = os.getenv("HNDLR", "!")
+
+bot = Client(SESSION, API_ID, API_HASH)
+self_or_contact_filter = filters.create(
+    lambda _, __, message:
+    (message.from_user and message.from_user.is_contact) or message.outgoing
+)
+call_py = PyTgCalls(bot)
+GROUP_CALL = []
+
+
+@bot.on_message(self_or_contact_filter & filters.command("cstream", prefixes=f"{HNDLR}"))
+async def stream(client, m: Message):
+   if len(m.command) < 2:
+      await m.reply("`Give A Link/LiveLink/.m3u8 URL/YTLink to Stream from 🎶`")
+   else:
+      link = m.text.split(None, 1)[1]
+      chat_id = m.chat.id
+      huehue = await m.reply("`Trying to Stream 💭`")
+
+      # Filtering out YouTube URL's
+      regex = r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+"
+      match = re.match(regex,link)
+      if match:
+         try:
+            meta = ydl.extract_info(link, download=False)
+            formats = meta.get('formats', [meta])
+            for f in formats:
+               ytstreamlink = f['url']
+            livelink = ytstreamlink
+         except Exception as e:
+            huehue.edit(f"**YTDL ERROR** \n{e}")
+      else:
+         livelink = link
+
+      # Playing
+      if chat_id in GROUP_CALL:
+         try:
+            await call_py.change_stream(
+               chat_id,
+               AudioVideoPiped(
+                  livelink,
+                  HighQualityAudio(),
+                  HighQualityVideo()
+               )
+            )
+            await huehue.delete()
+            await m.reply(f"Started [Streaming]({livelink}) in {chat_id}!", disable_web_page_preview=True)
+         except Exception as ep:
+            await huehue.edit(f"{ep}")
+      else:
+         try:
+            await call_py.join_group_call(
+               chat_id,
+               AudioVideoPiped(
+                  livelink,
+                  HighQualityAudio(),
+                  HighQualityVideo()
+               )
+            )
+            await huehue.delete()
+            await m.reply(f"Started [Streaming]({livelink}) in {chat_id}!", disable_web_page_preview=True)
+         except Exception as ep:
+            await huehue.edit(f"{ep}")
+      
+      
